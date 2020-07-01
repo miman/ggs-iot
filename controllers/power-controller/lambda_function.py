26d15d3b39d0db74f0ae43e908373c02b66cf203
Mikael <mikael.thorman@gmail.com>
2020-07-01 23:03:02 +0200

    logging fixes

--- controllers/power-controller/lambda_function.py
+++ controllers/power-controller/lambda_function.py
@@ -1,6 +1,10 @@
 # This lambda function will control the relay based on input on the topic "rfid/read" & "btn/state"
+#
 # If we get a msg on topic "rfid/read" we will activate the power
 # If we get a msg on topic "btn/state" we will de-activate the power
+#
+# We assume the Power relay is on PIN GPIO12
+# We assume the LED is on PIN GPIO13
 
 import logging
 import platform
@@ -11,29 +15,24 @@ from time import sleep
 import json
 import greengrasssdk
 
-print("power-controller> 1")
 # Setup logging to stdout
 logger = logging.getLogger(__name__)
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-print("power-controller> 2")
 # Creating a greengrass core sdk client
 client = greengrasssdk.client("iot-data")
 
-print("power-controller> 3")
-PWR_RELAY_PIN=12
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)    # Ignore warning for now
+
+PWR_RELAY_PIN=12
 GPIO.setup(PWR_RELAY_PIN, GPIO.OUT, initial=GPIO.HIGH)
 
-print("power-controller> 4")
 LEDPIN=13
 GPIO.setup(LEDPIN, GPIO.OUT, initial=GPIO.LOW)
 
-print("power-controller> 5")
-
 def rfid_msg_received(msg):
-    print("RFID msg received")
+    print("RFID msg received: Tag-Id: " + str(msg["id"]))
     try:
         GPIO.output(LEDPIN, GPIO.HIGH) # Turn on
         GPIO.output(PWR_RELAY_PIN, GPIO.LOW) # Turn on
@@ -48,15 +47,10 @@ def btn_msg_received(msg):
     except Exception as e:
         logger.error("Failed to handle btn msg: " + repr(e))
 
-# This is a dummy handler and will not be invoked
-# Instead the code above will be executed in an infinite loop for our example
-# context: https://docs.aws.amazon.com/lambda/latest/dg/python-context.html
+# This is the Lambda handler, this will be called whenever a msg is received on any topic that is routed to this Lambda
 def lambda_handler(event, context):
     print("power-controller> Msg received")
-    print("power-controller> context-ARN:" + context.invoked_function_arn)
-    print("power-controller> Msg content: " + json.dumps(event))
-    print("power-controller> aws_request_id :" + context.aws_request_id )
-    print("power-controller> custom :" + json.dumps(context.client_context.custom) )
+    # print("power-controller> Msg content: " + json.dumps(event))
     # parse received msg
     topic = context.client_context.custom["subject"]
     if topic == "rfid/read":
