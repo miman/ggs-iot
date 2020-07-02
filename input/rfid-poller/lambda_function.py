59301c93ac8f665e7670b5b71775a7811699f870
Mikael <mikael.thorman@gmail.com>
2020-07-02 08:22:49 +0200

    We now only send the RFID read event once

--- input/rfid-poller/lambda_function.py
+++ input/rfid-poller/lambda_function.py
@@ -11,6 +11,7 @@ from time import sleep
 from mfrc522 import SimpleMFRC522
 import json
 import greengrasssdk
+from datetime import datetime, timedelta
 
 # Setup logging to stdout
 logger = logging.getLogger(__name__)
@@ -22,15 +23,22 @@ client = greengrasssdk.client("iot-data")
 # Initiate RFID reader
 reader = SimpleMFRC522()
 
-device = os.environ['AWS_IOT_THING_NAME']
+# The name of this device
+device: str = os.environ['AWS_IOT_THING_NAME']
+
+# The last time a tag was read
+lastReadTime: datetime = datetime(2020, 1, 1, 0, 0)
+# The last Tag-Id that was read
+lastReadTag: str = ""
 
 # This is a dummy handler and will not be invoked
 # Instead the code above will be executed in an infinite loop for our example
 def lambda_handler(event, context):
     return
 
-def post_read_rfid_tag(id_no, text):
-    text_to_send="RFID tag read '" + str(id_no) + "' with content '" + text.strip() + "' on Greengrass device: " + device
+# Post that a new Tag was read to the MQTT topic "rfid/read"
+def post_read_rfid_tag(id_no: str, text: str):
+    text_to_send="Read RFID tag '" + str(id_no) + "' with content '" + text.strip() + "' on Greengrass device: " + device
     print(text_to_send)
     msg = {
         "id": id_no,
@@ -46,12 +54,22 @@ def post_read_rfid_tag(id_no, text):
     except Exception as e:
         logger.error("Failed to publish message: " + repr(e))
 
+# The main long lived loop function
 def start_read_cycle():
     print("Starting RFID RC522 reader")
+    global lastReadTime
+    global lastReadTag
     try:
         while True:
             id_no, text = reader.read()
-            post_read_rfid_tag(id_no, text)
+            now: datetime = datetime.now()
+            if id_no != lastReadTag or now > lastReadTime + timedelta(milliseconds=500):
+                # it is a new tag or the time sinca last read was more than 500 ms -> count as new read
+                lastReadTag = id_no
+                post_read_rfid_tag(id_no, text)
+            else:
+                print("Re-read the same RFID tag: " + str(id_no) + ", with text: " + text)
+            lastReadTime = now
             sleep(0.2) # Sleep for 0.2 second
 
     except Exception as e:
@@ -59,5 +77,5 @@ def start_read_cycle():
     finally:
         print("Closing RFID reader")
 
-# Start executing the function above
+# Start executing the long-lived polling-function
 start_read_cycle()
